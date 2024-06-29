4dfea1f0788357e4248974bafad989d11e5685b3
delguy2015 <delguy2015@gmail.com>
2024-06-29 14:39:58 -0400

    2. Smoke Test

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,17 +1,21 @@
-
 require("@nomiclabs/hardhat-waffle");
-
- /** @type import('hardhat/config').HardhatUserConfig */
+require("dotenv").config();
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
 module.exports = {
   solidity: "0.8.9",
   networks: {
-    localhost:{}
-  paths: {
-    sources: "./contracts",  // Default folder for contracts
-    tests: "./test",        // Default folder for tests
-    cache: "./cache",       // Default folder for cache
-    artifacts: "./artifacts"  // Default folder for artifacts
-  }
+    localhost: {}
+  },
 };
-
-
