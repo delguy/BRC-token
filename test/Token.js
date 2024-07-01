160403d195e5ac4cb0bbdc754c88ae28f8e1ff22
delguy2015 <delguy2015@gmail.com>
2024-07-01 18:31:30 -0400

    5. Token transfers

--- test/Token.js
+++ test/Token.js
@@ -6,7 +6,7 @@ const tokens = (n) => {
 }
 
 describe('Token', ()=> {
-	let token, accounts, deployer
+	let token, accounts, deployer, receiver
 	
 
 	beforeEach(async () => {
@@ -14,6 +14,7 @@ describe('Token', ()=> {
 		token = await Token.deploy('Dapp University','DAPP','1000000')
 		accounts = await ethers.getSigners()
 		deployer = accounts[0]
+		receiver = accounts[1]
 	})	
 
 	describe('Deployment', () => {
@@ -39,5 +40,57 @@ describe('Token', ()=> {
 	it('assigns total supply to deployer', async ()=> {
 		expect(await token.balanceOf(deployer.address)).to.equal(totalSupply)
 	}) 
+	})
+
+	describe('Sending Tokens', () => {
+		let amount
+
+	describe('Success',() => {
+
+	})
+
+	describe('Failure', () => {
+		it('rejects insufficient balances', async () => {
+
+			const invalidAmount = tokens(1000000000)
+			await expect (token.connect(deployer).transfer(receiver.address, invalidAmount)).to.be.reverted
+		})
+
+		it('rejects invalid recipent', async () => {
+
+			const amount = tokens(100)
+			await expect (token.connect(deployer).transfer(0x0000000000000000000000000000000, amount)).to.be.reverted
+	})
+
+
+	
+
+		beforeEach(async () => {
+			amount = tokens(100)
+			transaction = await token.connect(deployer).transfer(receiver.address, amount)
+			result = await transaction.wait()
+		})
+
+		it('tansfers token balances', async () => {
+			//transfer tokens
+			
+			// ensures that tokens were transfered (balance changed)
+			expect(await token.balanceOf(deployer.address)).to.equal(tokens(999900))
+			expect(await token.balanceOf(receiver.address)).to.equal(amount)
+			
+		})
+
+		it('emits a transfer event', async () => {
+			const event = result.events[0]
+			expect(event.event).to.equal('Transfer')
+
+			const args = event.args
+			expect(args.from).to.equal(deployer.address)
+			expect(args.to).to.equal(receiver.address)
+			expect(args.value).to.equal(amount)
+
+		})
+	})
+
 })
 })
