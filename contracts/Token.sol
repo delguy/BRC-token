8c5c087fdcdbe29baf2a133115059aa0ebf9b320
delguy2015 <delguy2015@gmail.com>
2024-07-02 16:56:28 -0400

    7. token transfer delegation

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -43,18 +43,24 @@ contract Token {
 		
 		//require sender has enough tokens
 		require(balanceOf[msg.sender] >= _value);
-		require(_to != address(0));
-
-		//deduct tokens from spender
-		balanceOf[msg.sender] = balanceOf[msg.sender] - _value;
-
-		//credit tokens to reciever	
-		balanceOf[_to] = balanceOf[_to] + _value;
-
-		emit Transfer(msg.sender, _to, _value);
-
+		
+		_transfer(msg.sender, _to, _value);
+	
 		return true;
  	}
+	function _transfer(
+		address _from,
+		address _to,
+		uint256 _value
+	) internal {
+		require(_to != address(0));
+
+		balanceOf[_from] = balanceOf[_from] - _value;
+		balanceOf[_to] = balanceOf[_to] + _value;
+
+		emit Transfer(_from, _to, _value);
+}
+
 
 	function approve(address _spender, uint256 _value) 
 		public 
@@ -66,6 +72,27 @@ contract Token {
 		emit Approval(msg.sender, _spender, _value);
 		return true;
 	}
+	function transferFrom(
+		address _from, 
+		address _to, 
+		uint256 _value) 
+		public
+		returns (bool success)
+	{
+		// check Approval
+		require(_value <= balanceOf[_from]);
+		require(_value <= allowance[_from][msg.sender]);
+		
+		//reset allowance
+		allowance[_from][msg.sender] = allowance[_from][msg.sender] - _value;
+
+		// spend
+		_transfer(_from, _to, _value);
+
+		return true;
+	}
+
+
 
 
 }
