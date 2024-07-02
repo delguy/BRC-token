315c7d330e50640afb9e3dda7876b12abe6d0aa1
delguy2015 <delguy2015@gmail.com>
2024-07-01 21:08:39 -0400

    6. Token approvals

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -10,10 +10,17 @@ contract Token {
 	uint256 public totalSupply;
 
 	mapping(address => uint256) public balanceOf;
+	mapping(address => mapping(address => uint256)) public allowance;
 
 	event Transfer(
 		address indexed from, 
 		address indexed to, 
+		uint256 value
+		);
+
+	event Approval(
+		address indexed owner,
+		address indexed spender,
 		uint256 value
 		);
 
@@ -34,18 +41,31 @@ contract Token {
 		returns (bool success) 
 		{
 		
-
 		//require sender has enough tokens
 		require(balanceOf[msg.sender] >= _value);
 		require(_to != address(0));
+
 		//deduct tokens from spender
 		balanceOf[msg.sender] = balanceOf[msg.sender] - _value;
+
 		//credit tokens to reciever	
 		balanceOf[_to] = balanceOf[_to] + _value;
 
 		emit Transfer(msg.sender, _to, _value);
 
 		return true;
- 
+ 	}
+
+	function approve(address _spender, uint256 _value) 
+		public 
+		returns(bool success)
+	{
+		require(_spender != address(0));
+
+		allowance[msg.sender][_spender] = _value;
+		emit Approval(msg.sender, _spender, _value);
+		return true;
 	}
+
+
 }
